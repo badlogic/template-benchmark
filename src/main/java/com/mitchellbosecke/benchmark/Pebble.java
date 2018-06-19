c4caebf215016191f19fe22fe7b4a37cc0b99d30
badlogic <contact@badlogicgames.com>
2018-06-20 00:52:08 +0200

    Make benchmark more fair.

--- src/main/java/com/mitchellbosecke/benchmark/Pebble.java
+++ src/main/java/com/mitchellbosecke/benchmark/Pebble.java
@@ -1,6 +1,7 @@
 
 package com.mitchellbosecke.benchmark;
 
+import com.mitchellbosecke.benchmark.model.Stock;
 import com.mitchellbosecke.pebble.PebbleEngine;
 import com.mitchellbosecke.pebble.error.PebbleException;
 import com.mitchellbosecke.pebble.template.PebbleTemplate;
@@ -9,24 +10,28 @@ import org.openjdk.jmh.annotations.Setup;
 
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Pebble extends BaseBenchmark {
 
-	private Map<String, Object> context;
-
 	private PebbleTemplate template;
+
+	private List<Stock> items;
 
 	@Setup
 	public void setup () throws PebbleException {
 		PebbleEngine engine = new PebbleEngine.Builder().autoEscaping(false).build();
 		template = engine.getTemplate("templates/stocks.pebble.html");
-		this.context = getContext();
+		items = Stock.dummyItems();
 	}
 
 	@Benchmark
 	public String benchmark () throws PebbleException, IOException {
 		StringWriter writer = new StringWriter();
+		Map<String, Object> context = new HashMap<String, Object>();
+		context.put("items", items);
 		template.evaluate(writer, context);
 		return writer.toString();
 	}
