c599a21b83c5f9f3a0291d4ddd975bfbc1b6eb07
badlogic <contact@badlogicgames.com>
2018-06-20 01:01:02 +0200

    Cleanup, updated all engines to their latest version.

--- src/test/java/com/mitchellbosecke/benchmark/ExpectedOutputTest.java
+++ src/test/java/com/mitchellbosecke/benchmark/ExpectedOutputTest.java
@@ -1,3 +1,4 @@
+
 package com.mitchellbosecke.benchmark;
 
 import static org.junit.Assert.assertEquals;
@@ -14,114 +15,108 @@ import com.mitchellbosecke.pebble.error.PebbleException;
 
 import freemarker.template.TemplateException;
 
-/**
- *
- * @author Martin Kouba
- */
+/** @author Martin Kouba */
 public class ExpectedOutputTest {
 
-    @BeforeClass
-    public static void beforeClass() {
-        Locale.setDefault(Locale.ENGLISH);
-    }
+	@BeforeClass
+	public static void beforeClass () {
+		Locale.setDefault(Locale.ENGLISH);
+	}
 
-    @Test
-    public void testFreemarkerOutput() throws IOException, TemplateException {
-        Freemarker freemarker = new Freemarker();
-        freemarker.setup();
-        assertOutput(freemarker.benchmark());
-    }
+	@Test
+	public void testFreemarkerOutput () throws IOException, TemplateException {
+		Freemarker freemarker = new Freemarker();
+		freemarker.setup();
+		assertOutput(freemarker.benchmark());
+	}
 
-    @Test
-    public void testPebbleOutput() throws IOException, PebbleException {
-        Pebble pebble = new Pebble();
-        pebble.setup();
-        assertOutput(pebble.benchmark());
-    }
+	@Test
+	public void testPebbleOutput () throws IOException, PebbleException {
+		Pebble pebble = new Pebble();
+		pebble.setup();
+		assertOutput(pebble.benchmark());
+	}
 
-    @Test
-    public void testVelocityOutput() throws IOException {
-        Velocity velocity = new Velocity();
-        velocity.setup();
-        assertOutput(velocity.benchmark());
-    }
+	@Test
+	public void testVelocityOutput () throws IOException {
+		Velocity velocity = new Velocity();
+		velocity.setup();
+		assertOutput(velocity.benchmark());
+	}
 
-    @Test
-    public void testMustacheOutput() throws IOException {
-        JavaMustache mustache = new JavaMustache();
-        mustache.setup();
-        assertOutput(mustache.benchmark());
-    }
+	@Test
+	public void testMustacheOutput () throws IOException {
+		JavaMustache mustache = new JavaMustache();
+		mustache.setup();
+		assertOutput(mustache.benchmark());
+	}
 
-    @Test
-    public void testJMustacheOutput() throws IOException {
-        JMustache mustache = new JMustache();
-        mustache.setup();
-        assertOutput(mustache.benchmark());
-    }
+	@Test
+	public void testJMustacheOutput () throws IOException {
+		JMustache mustache = new JMustache();
+		mustache.setup();
+		assertOutput(mustache.benchmark());
+	}
 
-    @Test
-    public void testThymeleafOutput() throws IOException, TemplateException {
-        Thymeleaf thymeleaf = new Thymeleaf();
-        thymeleaf.setup();
-        assertOutput(thymeleaf.benchmark());
-    }
+	@Test
+	public void testThymeleafOutput () throws IOException, TemplateException {
+		Thymeleaf thymeleaf = new Thymeleaf();
+		thymeleaf.setup();
+		assertOutput(thymeleaf.benchmark());
+	}
 
-    @Test
-    public void testTrimouOutput() throws IOException {
-        Trimou trimou = new Trimou();
-        trimou.setup();
-        assertOutput(trimou.benchmark());
-    }
+	@Test
+	public void testTrimouOutput () throws IOException {
+		Trimou trimou = new Trimou();
+		trimou.setup();
+		assertOutput(trimou.benchmark());
+	}
 
-    @Test
-    public void testHbsOutput() throws IOException {
-        Handlebars hbs = new Handlebars();
-        hbs.setup();
-        assertOutput(hbs.benchmark());
-    }
+	@Test
+	public void testHbsOutput () throws IOException {
+		Handlebars hbs = new Handlebars();
+		hbs.setup();
+		assertOutput(hbs.benchmark());
+	}
 
-    @Test
-    public void testJwtigOutput() throws IOException {
-   	 JTwig t = new JTwig();
-   	 t.setup();
-   	 System.out.println(t.benchmark());
-   	 assertOutput(t.benchmark());
-    }
+	@Test
+	public void testJwtigOutput () throws IOException {
+		JTwig t = new JTwig();
+		t.setup();
+		assertOutput(t.benchmark());
+	}
 
-    @Test
-    public void testBasisOutput() throws IOException {
-   	 BasisTemplate t = new BasisTemplate();
-   	 t.setup();
-   	 System.out.println(t.benchmark());
-   	 assertOutput(t.benchmark());
-    }
+	@Test
+	public void testBasisOutput () throws IOException {
+		BasisTemplate t = new BasisTemplate();
+		t.setup();
+		assertOutput(t.benchmark());
+	}
 
-    @Test
-    public void testBasisGettersOutput() throws IOException {
-   	 BasisTemplateGetters t = new BasisTemplateGetters();
-   	 t.setup();
-   	 System.out.println(t.benchmark());
-   	 assertOutput(t.benchmark());
-    }
+	@Test
+	public void testBasisGettersOutput () throws IOException {
+		BasisTemplateGetters t = new BasisTemplateGetters();
+		t.setup();
+		assertOutput(t.benchmark());
+	}
 
-    private void assertOutput(final String output) throws IOException {
-        assertEquals(readExpectedOutputResource(), output.replaceAll("\\s", ""));
-    }
+	private void assertOutput (final String output) throws IOException {
+		assertEquals(readExpectedOutputResource(), output.replaceAll("\\s", ""));
+	}
 
-    private String readExpectedOutputResource() throws IOException {
-        StringBuilder builder = new StringBuilder();
-        try (BufferedReader in = new BufferedReader(new InputStreamReader(ExpectedOutputTest.class.getResourceAsStream("/expected-output.html")))) {
-            for (;;) {
-                String line = in.readLine();
-                if (line == null) {
-                  break;
-                }
-                builder.append(line);
-            }
-        }
-        // Remove all whitespaces
-        return builder.toString().replaceAll("\\s", "");
-    }
+	private String readExpectedOutputResource () throws IOException {
+		StringBuilder builder = new StringBuilder();
+		try (BufferedReader in = new BufferedReader(new InputStreamReader(ExpectedOutputTest.class.getResourceAsStream("/expected-output.html")))) {
+			for (;;) {
+				String line = in.readLine();
+				if (line == null) {
+					break;
+				}
+				builder.append(line);
+			}
+		}
+		// Remove all whitespaces
+		return builder.toString().replaceAll("\\s", "");
+	}
 
 }
